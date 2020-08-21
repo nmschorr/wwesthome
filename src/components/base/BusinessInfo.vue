25754f10cec21800f5e6ffaef2d3c24b3b790242
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-08-21 14:48:34 -0700

    standard commit

---
+++ src/components/base/BusinessInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-theme-provider :dark="dark">
+    <div>
+      <base-info-card
+        title="KEEP IN TOUCH WITH US"
+        pace="6"
+      >
+        We love to keep in touch!
+      </base-info-card>
+
+      <base-business-contact dense />
+    </div>
+  </v-theme-provider>
+</template>
+
+<script>
+  export default {
+    name: 'BaseBusinessInfo',
+
+    props: { dark: Boolean },
+
+    data: () => ({
+      business: [
+        {
+          icon: 'mdi-map-marker-outline',
+          title: 'Address',
+          text: 'Singapore',
+        },
+        {
+          icon: 'mdi-cellphone',
+          title: 'Phone',
+          text: '01 (800) 555 744<br>01 (800) 555 633',
+        },
+        {
+          icon: 'mdi-email',
+          title: 'Email',
+          text: 'berzeck@nulstar.com',
+        },
+      ],
+    }),
+  }
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+// Imports
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  scrollBehavior: (to, from, savedPosition) => {
+    if (to.hash) return { selector: to.hash }
+    if (savedPosition) return savedPosition
+
+    return { x: 0, y: 0 }
+  },
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/layouts/home/Index.vue'),
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: () => import('@/views/home/Index.vue'),
+        },
+        {
+          path: 'about',
+          name: 'About',
+          component: () => import('@/views/about/Index.vue'),
+          meta: { src: require('@/assets/about.jpg') },
+        },
+        {
+          path: 'contact-us',
+          name: 'Contact',
+          component: () => import('@/views/contact-us/Index.vue'),
+          meta: { src: require('@/assets/contact.jpg') },
+        },
+        {
+          path: 'pro',
+          name: 'Pro',
+          component: () => import('@/views/pro/Index.vue'),
+          meta: { src: require('@/assets/pro.jpg') },
+        },
+        {
+          path: '*',
+          name: 'FourOhFour',
+          component: () => import('@/views/404/Index.vue'),
+        },
+      ],
+    },
+
+  ],
+})
+
+export default router
