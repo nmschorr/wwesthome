4bb0254ac40d274f3cf1349fa46eb1a5779bb08e
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-08-28 23:41:24 -0700

    standard commit

--- src/views/sections/Newsletter.vue
+++ src/views/sections/Newsletter.vue
@@ -10,19 +10,20 @@
         class="ma-0"
         justify="center"
       >
-        <base-title
-          space="0"
-          title="Invest in your future"
-        />
-        <v-img
-          src="@/assets/NULS_BANNER.svg"
-        />
-        <!-- <v-responsive
+        <v-responsive
           class="px-4 mx-6"
           max-width="600"
           width="100%"
         >
-          <base-text-field
+          <v-img
+            :src="`../../assets/Community_Tree.svg;`"
+            min-height="200px"
+          />
+          <base-title
+            space="0"
+            title="Invest in your future"
+          />
+          <!--  <base-text-field
             hide-details
             label="Your Email Address"
           >
@@ -36,8 +37,8 @@
                 Subscribe
               </v-btn>
             </template>
-          </base-text-field>
-        </v-responsive> -->
+          </base-text-field> -->
+        </v-responsive>
       </v-row>
     </base-section>
   </v-theme-provider>
